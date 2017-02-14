135cbe0a243540747f98854f46fcfd300957e637
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2017-02-14 15:36:49 -0300

    Upload for testing online

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+	createCanvas(600,400);
+}
+
+function draw() {
+var k = 20;
+	//background(0,0,0);
+  	//background(random(255),random(255),random(255));
+	stroke(255,255,255);
+	line(k*2,k*1,k*4,k*5);
+	line(k*2,k*4,k*4,k*8);
+	rect(0,0,20,40);
+	if(mouseIsPressed){
+	stroke(random(255),random(255),random(255));
+	fill(random(255),random(255),random(255));
+	ellipse(mouseX,mouseY,20,40);
+	}
+}
